5f0add485ab259907b9ae7ced0604b00cdef37c2
Zzacklack <149129501+Zzackllack@users.noreply.github.com>
2024-05-17 01:40:35 +0200

    Create Auto.java

---
+++ UML/Auto.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * Description
+ *
+ * @version 1.0 from 17.05.2024
+ * @author 
+ */
+
+public class Auto extends Fahrzeug {
+  
+  // start attributes
+  private String marke;
+  // end attributes
+  
+  // start methods
+  public String getMarke() {
+    return marke;
+  }
+
+  // end methods
+} // end of Auto
