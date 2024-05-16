98b611975d0fd229658c607d39f88e19a72a5148
Zzacklack <149129501+Zzackllack@users.noreply.github.com>
2024-05-17 01:41:32 +0200

    Add files via upload

---
+++ UML/Strasse.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * Description
+ *
+ * @version 1.0 from 17.05.2024
+ * @author 
+ */
+
+public class Strasse {
+  
+  // start attributes
+  public static String args;
+  // end attributes
+  // start methods
+  // end methods
+} // end of Strasse
