32826527812225c868a4e3f1a82ba34c65badc34
Zzacklack <149129501+Zzackllack@users.noreply.github.com>
2024-05-21 01:33:54 +0200

    Update Strasse.java

--- UML/Strasse.java
+++ UML/Strasse.java
@@ -2,15 +2,16 @@
  *
  * Description
  *
- * @version 1.0 from 17.05.2024
+ * @version 1.0 from 21.05.2024
  * @author 
  */
 
 public class Strasse {
   
-  // start attributes
-  public static String args;
-  // end attributes
-  // start methods
-  // end methods
+  public static void main(String[] args)
+  {
+         Lehrkraft cool = new Lehrkraft("cool", 33);
+         cool.autoKaufen(new Auto("mercedes", 50, 280, 20, 70));
+         cool.print();
+  }
 } // end of Strasse
