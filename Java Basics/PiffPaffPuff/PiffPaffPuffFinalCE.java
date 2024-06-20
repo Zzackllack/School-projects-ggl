2f3eb32a44d520a880281079394e93af8e589160
Zzacklack <149129501+Zzackllack@users.noreply.github.com>
2024-06-20 23:55:26 +0200

    Add files via upload

---
+++ Java Basics/PiffPaffPuff/PiffPaffPuffFinalCE.java
@@ -0,0 +1,11 @@
+public class PiffPaffPuffFinalCE {
+    public static void main(String[] args) {
+        Piff piffObjekt = new Piff();
+        Paff paffObjekt = new Paff();
+        Puff puffObjekt = new Puff();
+
+        piffObjekt.Ausgabe();
+        paffObjekt.Ausgabe();
+        puffObjekt.Ausgabe();
+    }
+}
