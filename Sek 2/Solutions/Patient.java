20308dbb6bbc6d862d4ab9ac9686229a3f1073fd
Zacklack <contact@zacklack.de>
2024-10-05 23:35:41 +0200

    Solution for Patient.java

---
+++ Sek 2/Solutions/Patient.java
@@ -0,0 +1,22 @@
+public class Patient {
+	//Dies ist eine Datenklasse; nur get- und set-Methoden
+	private String nachname; 
+	private int alter;
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public int getAlter() {
+		return alter;
+	}
+
+	public void setAlter(int alter) {
+		this.alter = alter;
+	}
+
+}
