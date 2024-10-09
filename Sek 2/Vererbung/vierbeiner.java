cc877926f6925554af7e0ec5839e6bc440e8c7ec
Zacklack <contact@zacklack.de>
2024-10-09 23:51:17 +0200

    Created the main class to make inheritance possible

---
+++ Sek 2/Vererbung/vierbeiner.java
@@ -0,0 +1,16 @@
+public class vierbeiner {
+	String vierbeinerName;
+	public void rennen() {
+		System.out.println(vierbeinerName+" rennt los!");
+	}
+	public int funktioniert(int a, int b) {
+		System.out.println("funktioniert");
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
