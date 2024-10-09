427c6f19c172a61d11c01307a3524582b0a1890f
Zacklack <contact@zacklack.de>
2024-10-09 23:49:24 +0200

    Did the Hund.java task

---
+++ Sek 2/Vererbung/Hund.java
@@ -0,0 +1,12 @@
+public class Hund extends vierbeiner {
+	
+	public void bellen() {
+		System.out.println("wuff~");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
