473c9ee6e1e8296cf2f0758269401989cff3c183
Zacklack <contact@zacklack.de>
2024-10-09 23:50:02 +0200

    Did the task for Katze.java

---
+++ Sek 2/Vererbung/Katze.java
@@ -0,0 +1,20 @@
+public class Katze extends vierbeiner {
+	
+	public void maus_jagen() {
+		System.out.println("Die Katze jagt die Maus.");
+
+		}
+	public short einsodernull(short a) {
+		if(a==0) {
+			System.out.println("es ist null");
+		}else {
+			System.out.println("es ist nicht null");
+		}
+		return a;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
