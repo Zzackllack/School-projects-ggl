acc44fa8957500222455aad03fadab9fae264dd2
Zacklack <contact@zacklack.de>
2024-10-16 22:10:37 +0200

    Did the second Task to interfaces

---
+++ Sek 2/Interfaces/2-Test/Constant.java
@@ -0,0 +1,4 @@
+public interface Constant {
+    int MAX_VALUE = 100;
+    int MIN_VALUE = 0;
+}
---
+++ Sek 2/Interfaces/2-Test/KonstantenTest.java
@@ -0,0 +1,7 @@
+public class KonstantenTest implements Constant {
+    public static void main(String[] args) {
+        System.out.println("Max Value: " + MAX_VALUE);
+        System.out.println("Min Value: " + MIN_VALUE);
+    }
+}
+
