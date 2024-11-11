15422fb98aa5e89cfc05230ad6f69bdf81ad568f
Zacklack <contact@zacklack.de>
2024-11-11 10:20:45 +0100

    Testing without Generics (before Java 5) //Task from 11.11.2024

---
+++ Sek 2/Generics/OldStyleOhneGenerics.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OldStyleOhneGenerics {
+    public static void main(String[] args) {
+		final List laeuferList = new ArrayList();
+		laeuferList.add(new Laeufer("Max", 44, true));
+		laeuferList.add(new Laeufer("Moritz", 42, true));
+		laeuferList.add(new Laeufer("Ingo", 64, false));
+		laeuferList.add(new Dog("I am a dog, not a runner!!!"));
+
+		for (int i =0; i<laeuferList.size();i++) {
+			Laeufer laeufer = (Laeufer) laeuferList.get(i);
+			System.out.println(laeufer.getName() + " ist qualifiziert: " + laeufer.isQuali());
+		}
+	}
+}
