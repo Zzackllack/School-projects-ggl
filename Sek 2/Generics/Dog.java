d6781f80a52ed26e5997f794f8803852254ab3d3
Zacklack <contact@zacklack.de>
2024-11-11 10:16:36 +0100

    Added Dog class //Task from 11.11.2024

---
+++ Sek 2/Generics/Dog.java
@@ -0,0 +1,16 @@
+public class Dog {
+	private String name;
+	
+	public Dog(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	
+	public void setName(String name) {
+		 this.name = name;
+	}
+}
