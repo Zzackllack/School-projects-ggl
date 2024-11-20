b2909e26c8f471cb67ef44861e3e1df5147ef4ca
Zacklack <contact@zacklack.de>
2024-11-20 21:44:49 +0100

    Fix initialization and typo in Kartenspiel; improve karteVomStapelNehmen method and enhance obersteKarteAusgeben output

--- Sek 2/Stack/Kartenspiel.java
+++ Sek 2/Stack/Kartenspiel.java
@@ -1,22 +1,32 @@
 public class Kartenspiel {
-	private Stack<Karte> kartenstapel;
-	private int zaelher;
-	
-	public Kartenspiel() {
-		kartenstapel = new Stack<Karte>();
-		int zaehler = 0;
-	}
+    private Stack<Karte> kartenstapel;
+    private int zaehler;  // Fixed typo
+    
+    public Kartenspiel() {
+        kartenstapel = new Stack<Karte>();
+        zaehler = 0;  // Fixed initialization
+    }
 
-	public void karteAufStapelLegen(Karte pKarte){
-		kartenstapel.push(pKarte);
-	}
-	
- 	public void karteVomStapelNehmen(Karte karte) {
- 		kartenstapel.pop();
- 		zaelher--;
- 	}
- 	
- 	public void obersteKarteAusgeben() {
- 		kartenstapel.top();
- 	}
+    public void karteAufStapelLegen(Karte pKarte) {
+        kartenstapel.push(pKarte);
+        zaehler++;
+    }
+    
+    public void karteVomStapelNehmen() {  // Removed unnecessary parameter
+        if (!kartenstapel.isEmpty()) {
+            kartenstapel.pop();
+            zaehler--;
+        }
+    }
+    
+    public void obersteKarteAusgeben() {
+        Karte obersteKarte = kartenstapel.top();
+        if (obersteKarte != null) {
+            System.out.println("Oberste Karte:");
+            System.out.println("Muster: " + obersteKarte.getMuster());
+            System.out.println("Aufgedeckt: " + obersteKarte.isKartenaufgedeckt());
+        } else {
+            System.out.println("Der Kartenstapel ist leer.");
+        }
+    }
 }
