63e182f43da2bce0306ae69c21c87d676b4645ff
Zacklack <contact@zacklack.de>
2024-12-02 11:47:15 +0100

    Add Artikel class with a constructor and getter for name

---
+++ Sek 2/List/Artikel.java
@@ -0,0 +1,12 @@
+public class Artikel {
+private String Name;
+
+    public Artikel(String name) {
+        this.Name = name;
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+}
