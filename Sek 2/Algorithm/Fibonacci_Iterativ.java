ed63e86b0fb326b4b10619b591ebf14be401d696
Zacklack <contact@zacklack.de>
2024-12-16 23:44:03 +0100

    Add Fibonacci_Iterativ class for iterative computation of Fibonacci numbers

---
+++ Sek 2/Algorithm/Fibonacci_Iterativ.java
@@ -0,0 +1,20 @@
+public class Fibonacci_Iterativ {
+
+    public int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Fibonacci_Iterativ f = new Fibonacci_Iterativ();
+        System.out.println(f.fibonacci(10));
+    }
+}
