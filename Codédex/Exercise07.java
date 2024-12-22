d54720d85ea31a00b7a8688efbacee2d16598d44
Zacklack <contact@zacklack.de>
2024-12-22 23:15:48 +0100

    Did Exercise 07

---
+++ Codédex/Exercise07.java
@@ -0,0 +1,17 @@
+package Codédex;
+
+public class Exercise07 {
+    public static void main(String[] args) {
+        String name = "Cédric";
+        String state = "Berlin";
+        char gender = 'M';
+        int age = 16;
+        boolean organDonor = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("State: " + state);
+        System.out.println("Gender: " + "gender");
+        System.out.println("Age: " + age);
+        System.out.println("Organ Donor: " + organDonor);
+    }
+}
