0ddff914601dc567b7689ca426ac90761aa9092a
Zacklack <contact@zacklack.de>
2024-12-22 23:07:26 +0100

    Did Exercise 04 -- error message handling in Java

---
+++ Codédex/Java/Exercise04.java
@@ -0,0 +1,14 @@
+package Codédex.Java;
+
+public class Exercise04 {
+    public static void main(String[] args) {
+
+        System.err.println("Connecting to server…"); 
+        System.err.println("Error: Connection timed out."); 
+        System.err.println("Retrying…"); 
+
+        // System.err.println() is used to print error messages! 😵😵❌
+        // It is similar to System.out.println() but is used to print error messages.
+
+    }
+}
