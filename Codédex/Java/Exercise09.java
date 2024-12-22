2cd9d6d0e4e869c7011ea312d1ed34cd98717150
Zacklack <contact@zacklack.de>
2024-12-22 23:27:06 +0100

    Did Exercise 09

---
+++ Codédex/Java/Exercise09.java
@@ -0,0 +1,18 @@
+package Codédex.Java;
+import java.util.Scanner;
+
+public class Exercise09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?");
+        String answer = scanner.nextLine();
+        String correctAnswer = "Echo";
+        
+        if (answer.equalsIgnoreCase(correctAnswer)){
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Incorrect! The correct answer is Echo.");
+        }
+    }
+}
