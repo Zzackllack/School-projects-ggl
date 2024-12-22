39f18fddf9c0ddf028cdea686a522985de544972
Zacklack <contact@zacklack.de>
2024-12-22 23:09:34 +0100

    Did Exercise 05

---
+++ Codédex/Java/Exercise05.java
@@ -0,0 +1,23 @@
+package Codédex.Java;
+
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh?
+ * Write a little bit about yourself here. What's your name? Where are you from? What do you do? -NO
+ *  
+ * I'm a comment, and I want to say hi! 👋
+ */
+
+ public class Exercise05 {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+
+        System.out.println("Blowing Smoke"); // Gracie Abrams
+        System.out.println("Amoeba"); // Clairo
+        System.out.println("Fantasy"); // Luna Li
+        System.out.println("Bad Dream"); // Wallows
+        System.out.println("THE BOTTOM"); // MICHELLE
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
+
