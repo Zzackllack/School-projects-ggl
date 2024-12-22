471c5ccd385dfbcd8a9480681532601873bb69a1
Zacklack <contact@zacklack.de>
2024-12-22 23:32:19 +0100

    Renamed first two exercises

--- Codédex/Java/Exercise01.java
+++ Codédex/Java/Exercise01.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Exercise01 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
--- Codédex/Java/Exercise02.java
+++ Codédex/Java/Exercise02.java
@@ -1,6 +1,6 @@
 package Codédex.Java;
 
-public class HelloStranger {
+public class Exercise02 {
     public static void main(String[] args) {
         System.out.println("Hello, Stranger!");
     }
