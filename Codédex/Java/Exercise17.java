b9ffcb472bb27c76759210dc7b86eb4ed9152d7e
Zacklack <contact@zacklack.de>
2024-12-27 22:51:58 +0100

    Did Exercise 17: While loop ^2

---
+++ Codédex/Java/Exercise17.java
@@ -0,0 +1,11 @@
+public class Exercise17 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        while(a<=15){
+            System.out.println("Line " + a + " LET'S Go <TEAM>!");
+            a++;
+        }
+        
+    }
+}
