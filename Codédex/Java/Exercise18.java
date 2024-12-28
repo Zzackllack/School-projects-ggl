66aba790af66c824f41b2e5c6eb27c483a457a86
Zacklack <contact@zacklack.de>
2024-12-28 22:51:55 +0100

    Did Exercise 18: Affirmations

---
+++ Codédex/Java/Exercise18.java
@@ -0,0 +1,7 @@
+public class Exercise18 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("I will be great at programming! 🚀"); 
+        } 
+    }
+}
