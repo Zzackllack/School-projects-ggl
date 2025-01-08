b40545be8b22dfa71aaccf3d81e9c7e0db6ad245
Zacklack <contact@zacklack.de>
2025-01-08 22:44:35 +0100

    Refactor suchen class to initialize list and add method for appending elements; include main method for testing linear search functionality - Task from 8.1.25

--- Sek 2/Algorithm/Linear search/suchen.java
+++ Sek 2/Algorithm/Linear search/suchen.java
@@ -1,10 +1,10 @@
 public class suchen {
     private List<Integer> liste;
 
-    public suchen(List<Integer> liste){
-        this.liste = liste;
+    public suchen() {
+        liste = new List<>();
     }
-
+    
     public boolean lineareSuche(int i)
     {
         boolean gefunden = false;
@@ -17,5 +17,25 @@ public class suchen {
         }
     return gefunden;
     }
-    
+
+    public void add(int i){
+        liste.append(i);
+    }
+
+    public static void main(String[] args) {
+        suchen s = new suchen();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(5);
+        s.add(6);
+        s.add(7);
+        s.add(8);
+        s.add(9);
+        s.add(10);
+        System.out.println(s.lineareSuche(5));
+        System.out.println(s.lineareSuche(11));
+    }
+
 }
