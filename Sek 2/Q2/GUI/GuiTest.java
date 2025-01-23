9f88bd5aa46b2369d4609f63506ee46a985b8324
Zacklack <contact@zacklack.de>
2025-01-23 23:07:05 +0100

    Did the Task from 20.1.25 - Additional components: text field, check box, radio buttons, combo box, and slider

--- Sek 2/Q2/GUI/GuiTest.java
+++ Sek 2/Q2/GUI/GuiTest.java
@@ -13,6 +13,25 @@ public class GuiTest {
         JButton button = new JButton("Klick mich");
         frame.add(button);
 
+        JTextField textField = new JTextField(15);
+        frame.add(textField);
+
+        JCheckBox checkBox = new JCheckBox("Mich ankreuzen");
+        frame.add(checkBox);
+        JRadioButton radioButton1 = new JRadioButton("Option 1");
+        JRadioButton radioButton2 = new JRadioButton("Option 2");
+        ButtonGroup group = new ButtonGroup();
+        group.add(radioButton1);
+        group.add(radioButton2);
+        frame.add(radioButton1);
+        frame.add(radioButton2);
+
+        JComboBox<String> comboBox = new JComboBox<>(new String[]{"Item 1", "Item 2", "Item 3"});
+        frame.add(comboBox);
+
+        JSlider slider = new JSlider(0, 100, 50);
+        frame.add(slider);
+
         frame.setLayout(new FlowLayout());
         frame.setVisible(true);
     }
