b0007907cdedde77fc7b641a1000efffe2192d33
Zacklack <contact@zacklack.de>
2025-03-17 21:16:22 +0100

    Task from 17.03.25 - Nested Classes, 4.2

---
+++ Sek 2/Q2/GUI/GUI-InnerClass/MyOuterClass.java
@@ -0,0 +1,4 @@
+public class MyOuterClass {   // outer class defined here
+   private class MyNestedClass1 { ...... }  // an nested class defined inside the outer class
+   public static class MyNestedClass2 { ...... }  // an "static" nested class defined inside the outer class
+}
