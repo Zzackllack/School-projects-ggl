6bafd05178c70de08edb032b13725d714cccf441
Zzackllack <contact@zacklack.de>
2025-09-11 11:58:31 +0200

    added missing functions

--- Sek 2/Q3/NLDS/BinarySearchTree/src/Benutzerverwaltung.java
+++ Sek 2/Q3/NLDS/BinarySearchTree/src/Benutzerverwaltung.java
@@ -1,8 +1,18 @@
 public class Benutzerverwaltung{
+
     private BinarySearchTree<Benutzerprofil> benutzerBaum = new
     BinarySearchTree<Benutzerprofil>();
     public void neuenNutzerAnlegen(String pBenutzername, String pPw){
         Benutzerprofil b = new Benutzerprofil(pBenutzername, pPw);
         benutzerBaum.insert(b);
     }
+
+    public void nutzerLoeschen(String pBenutzername, String pPw){//TODO}
+    }
+
+    public boolean profilVorhanden(String pBenutzername){
+        Benutzerprofil b = new Benutzerprofil(pBenutzername, "");
+        return benutzerBaum.contains(b);
+    }
+    
 }
