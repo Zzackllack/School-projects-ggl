c565daf9d53f1954e1976a941b6c2dcec3d1f42a
Zzackllack <contact@zacklack.de>
2026-01-14 10:39:22 +0100

    docs(mystic): update function description for clarity

--- Sek 2/Q3/Functional-Programming/Haskell/Functions_1/src/mystic.hs
+++ Sek 2/Q3/Functional-Programming/Haskell/Functions_1/src/mystic.hs
@@ -1,3 +1,4 @@
 -- Aufgabe 1
+-- Die Funktion mystic nimmt zwei Integer-Werte a und b als Eingabe und berechnet: (a / 2) + (3*a - b). Aufbau: Die erste Zeile (mystic :: Int -> Int -> Int) ist die Typsignatur - sie definiert, dass die Funktion zwei Integer-Parameter nimmt und einen Integer zurückgibt. Die zweite Zeile ist die Funktionsdefinition mit den Parametern a und b und der Berechnungsformel.
 mystic :: Int -> Int -> Int
 mystic a b = div a 2 +(3*a - b)
