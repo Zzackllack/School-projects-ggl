ba1d72b405c30fa84ad3729fbe107e6762e9b16e
Zzackllack <contact@zacklack.de>
2026-01-14 11:14:41 +0100

    docs(welcome): add comment about syntax errors in welcome function

--- Sek 2/Q3/Functional-Programming/Haskell/Guards/src/welcome.hs
+++ Sek 2/Q3/Functional-Programming/Haskell/Guards/src/welcome.hs
@@ -1,3 +1,5 @@
+-- Aufgabe 1, es waren syntaxfehler enthalten
+
 welcome :: String -> String -> String
 welcome vorname nachname
     | vorname == "Steffen" && nachname == "Jost" = "Good morning Dr Jost!"
