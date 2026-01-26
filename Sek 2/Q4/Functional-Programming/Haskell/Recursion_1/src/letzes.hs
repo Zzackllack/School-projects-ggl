956097b70f5b01ff8b1b618622f4794391e5a6ad
Zzackllack <contact@zacklack.de>
2026-01-26 09:22:27 +0100

    docs(letztes): add detailed comments for letztes function implementation

--- Sek 2/Q4/Functional-Programming/Haskell/Recursion_1/src/letzes.hs
+++ Sek 2/Q4/Functional-Programming/Haskell/Recursion_1/src/letzes.hs
@@ -1,4 +1,18 @@
+-- Gibt das letzte Element einer Liste zurück.
+-- Typannotation: für jede Liste von Werten des Typs 'a' liefert die Funktion ein 'a'.
 letztes :: [a] -> a
+
+-- Fall 1: leere Liste
+-- Eine leere Liste hat kein letztes Element, daher verursachen wir einen Fehler
 letztes []     = error "letztes: leere Liste"
+
+-- Fall 2: Liste mit genau einem Element
+-- Wenn die Liste genau ein Element [x] ist, ist dieses Element das letzte.
 letztes [x]    = x
+
+-- Fall 3: Liste mit mindestens zwei Elementen
+-- Der Ausdruck (_:xs) zerlegt die Liste in Kopf (hier unbenutzt, deshalb '_')
+-- und Schwanz 'xs'. Wir rufen 'letztes' rekursiv auf dem Schwanz auf.
+-- Dadurch wird die Liste bei jedem Aufruf kürzer, bis eines der obigen
+-- Basisfälle erreicht wird.
 letztes (_:xs) = letztes xs
